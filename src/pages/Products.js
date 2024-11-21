dca6760bb57fcd799150787badd0a827f5f5b1e0
olfi-mcbp <olaf.tarlowski@gmail.com>
2024-11-21 11:19:39 +0100

    clean up

--- src/pages/Products.js
+++ src/pages/Products.js
@@ -1,69 +1,55 @@
-import {
-  bri60,
-  bri90,
-  eliquis25,
-  eliquis5,
-  forxiga5,
-  forxiga10,
-  pradaxa75,
-  pradaxa150,
-  xarelto15,
-  xarelto20,
-  xigduo,
-} from "../assets/products-img";
-import { ProductItem } from "../components";
 import ProductTabs from "../components/ProductTabs";
 
 import { ProductsWrapper } from "../styled-components/style";
 
 const Products = () => {
-  const productsData = [
-    {
-      name: "Brilique 60mg",
-      img: bri60,
-    },
-    {
-      name: "Brilique 90mg",
-      img: bri90,
-    },
+  // const productsData = [
+  //   {
+  //     name: "Brilique 60mg",
+  //     img: bri60,
+  //   },
+  //   {
+  //     name: "Brilique 90mg",
+  //     img: bri90,
+  //   },
 
-    {
-      name: "Eliquis 2,5mg",
-      img: eliquis25,
-    },
-    {
-      name: "Eliquis 5mg",
-      img: eliquis5,
-    },
-    {
-      name: "Forxiga 5mg",
-      img: forxiga5,
-    },
-    {
-      name: "Forxiga 10mg",
-      img: forxiga10,
-    },
-    {
-      name: "Pradaxa 75mg",
-      img: pradaxa75,
-    },
-    {
-      name: "Pradaxa 150mg",
-      img: pradaxa150,
-    },
-    {
-      name: "Xarelto 15mg",
-      img: xarelto15,
-    },
-    {
-      name: "Xarelto 20mg",
-      img: xarelto20,
-    },
-    {
-      name: "Xigduo",
-      img: xigduo,
-    },
-  ];
+  //   {
+  //     name: "Eliquis 2,5mg",
+  //     img: eliquis25,
+  //   },
+  //   {
+  //     name: "Eliquis 5mg",
+  //     img: eliquis5,
+  //   },
+  //   {
+  //     name: "Forxiga 5mg",
+  //     img: forxiga5,
+  //   },
+  //   {
+  //     name: "Forxiga 10mg",
+  //     img: forxiga10,
+  //   },
+  //   {
+  //     name: "Pradaxa 75mg",
+  //     img: pradaxa75,
+  //   },
+  //   {
+  //     name: "Pradaxa 150mg",
+  //     img: pradaxa150,
+  //   },
+  //   {
+  //     name: "Xarelto 15mg",
+  //     img: xarelto15,
+  //   },
+  //   {
+  //     name: "Xarelto 20mg",
+  //     img: xarelto20,
+  //   },
+  //   {
+  //     name: "Xigduo",
+  //     img: xigduo,
+  //   },
+  // ];
 
   return (
     <ProductsWrapper>
